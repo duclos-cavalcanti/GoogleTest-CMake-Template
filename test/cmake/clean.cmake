bc42a05b7aa2c4c90cf13f49ec3af3117ddad09f
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-08-23 21:28:56 +0200

    Adding bin and build folders to test

--- test/cmake/clean.cmake
+++ test/cmake/clean.cmake
@@ -3,5 +3,6 @@ add_custom_target(
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/build/
         COMMENT "Cleaning Build Files"
         COMMAND rm -rf ${PROJECT_SOURCE_DIR}/build/*
+        COMMAND [ -d ${PROJECT_SOURCE_DIR}/build/.cache ] && rm -rf ${PROJECT_SOURCE_DIR}/build/*
         COMMAND touch .gitkeep
 )
